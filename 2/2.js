6c7c0b1ca8bb735310b7e4fbb480385d78d5ae9e
Kamal587 <kamalbugaldinov@rambler.ru>
2022-05-29 13:23:41 +0600

    mod_7

--- 2/2.js
+++ 2/2.js
@@ -1,21 +1,19 @@
 'use strict'
 
-function strObj(str, obj){
-    for(let key in obj){
-        if(obj.hasOwnProperty(key) && key == str){
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
-const obj = {
-    mike: 12, 
-    lysi: 15,
-    jack: 11,
-}
-
-console.log(strObj('mike', obj))
+let user = {
+    name: "John",
+    surname: "Smith",
+    num: 10,
+    bull: false
+  };
+  
+  function strObj(str, obj) {
+    let values = Object.values(obj);
+    return values.includes(str);
+    
+  };
+  
+  let res = strObj('Smith', user);
+  console.log(res);
 
 
